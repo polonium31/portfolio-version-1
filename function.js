7210dea8bff2f8e3256a78191b24facc483494de
polonium31 <jenu3181@gmail.com>
2021-07-06 15:11:05 +0530

    v1

---
+++ function.js
@@ -0,0 +1,5 @@
+<script type="text/javascript" src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+$(document).ready(function() {
+var width=(1/5*100);
+$('#experience-bar-progress').css('width','=width + "%"');
+});
